1a00ca0fbf6e1f65768cfb3a0ac384e632479781
rohan209547mourya <rohan.209547@gmail.com>
2024-04-24 16:12:38 +0530

    created dice roller

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let dice1 = Math.floor(Math.random() * 6) + 1;
-let dice2 = Math.floor(Math.random() * 6) + 1;
+function rollDice() {
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let dice2 = Math.floor(Math.random() * 6) + 1;
 
+    let dice1Image = document.getElementById("dice-1");
+    let dice2Image = document.getElementById("dice-2");
 
-
-function refreshPage() {
-    window.location.reload();
+    dice1Image.setAttribute("src", `./images/dice${dice1}.png`);
+    dice2Image.setAttribute("src", `./images/dice${dice2}.png`);
 }
